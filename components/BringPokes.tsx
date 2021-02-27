2b50877ceee7c05f22324884321e6bffd235a71d
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-02-27 15:19:35 -0300

    ok

--- components/BringPokes.tsx
+++ components/BringPokes.tsx
@@ -1,10 +1,17 @@
 import styles from '../styles/BringPokes.module.css'
+import db from '../db.json'
+import { useState } from 'react'
 
 export const BringPokes = () => {
+
+const [pokemon,setPokemon] = useState("");
+
     return (
         <div className={styles.pokeForm}>
             <form>
-                <input/>
+                <input
+                placeholder={db.inputplaceholder}
+                />
             </form>
         </div>
     )
