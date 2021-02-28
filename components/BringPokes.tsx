777c642eff1ba042b589e51fa6596e47d28babce
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-02-28 01:15:38 -0300

    key

--- components/BringPokes.tsx
+++ components/BringPokes.tsx
@@ -8,68 +8,72 @@ export const BringContext = createContext({});
 
 export const BringPokes = () => {
 
-const [pokemon,setPokemon] = useState([]);
-const [currentUrl,setCurrentUrl] = useState(`https://pokeapi.co/api/v2/pokemon`);
-const [nextUrl,setNextUrl] = useState();
-const [previousUrl,setpreviousUrl] = useState();
-const [isLoading,setIsloading] = useState(true);
+    const [pokemon, setPokemon] = useState([]);
+    const [currentUrl, setCurrentUrl] = useState(`https://pokeapi.co/api/v2/pokemon`);
+    const [nextUrl, setNextUrl] = useState();
+    const [previousUrl, setpreviousUrl] = useState();
+    const [isLoading, setIsloading] = useState(true);
 
-const fetchingPokes = async () => {
-    setIsloading(true)
-    let cancel
-    const res = await axios.get(currentUrl,{
-        cancelToken: new axios.CancelToken(c => cancel = c)
-    });
-    const data = await res.data;
-    console.log(data.results);
-    setTimeout(()=>{
-        setIsloading(false)
-    },400)
-    setNextUrl(data.next)
-    setpreviousUrl(data.previous)
-    setPokemon(data.results)
-    return () => cancel();
-}
+    const fetchingPokes = async () => {
+        setIsloading(true)
+        let cancel
+        const res = await axios.get(currentUrl, {
+            cancelToken: new axios.CancelToken(c => cancel = c)
+        });
+        const data = await res.data;
+        console.log(data.results);
+        setTimeout(() => {
+            setIsloading(false)
+        }, 400)
+        setNextUrl(data.next)
+        setpreviousUrl(data.previous)
+        setPokemon(data.results)
+        return () => cancel();
+    }
 
-const loadingPokes = async () => {
-    await axios.get(pokemon.url)
-}
+    const loadingPokes = () => {
+        pokemon.forEach((item) => {
+            axios.get(item.url)
+                .then(res => console.log(res.data))
+        })
+    }
 
-useEffect(()=>{
-    fetchingPokes();
-},[currentUrl])
+    useEffect(() => {
+        loadingPokes();
+        fetchingPokes();
+    }, [currentUrl])
 
-const nextPage = () => {
-    setCurrentUrl(nextUrl)
-}
+    const nextPage = () => {
+        setCurrentUrl(nextUrl)
+    }
 
-const prevPage = () => {
-    setCurrentUrl(previousUrl)
-}
+    const prevPage = () => {
+        setCurrentUrl(previousUrl)
+    }
 
-if (isLoading) return (
-    <div className={styles.loadingContainer}>
-    <img src="/loading.gif"/>
-    <p>Loading ...</p>
-    </div>)
+    if (isLoading) return (
+        <div className={styles.loadingContainer}>
+            <img src="/loading.gif" />
+            <p>Loading ...</p>
+        </div>)
 
     return (
         <div className={styles.pokeForm}>
             <form>
                 <input
-                placeholder={db.inputplaceholder}
+                    placeholder={db.inputplaceholder}
                 />
             </form>
             <div>
-                {pokemon.map(item=>(
-        <div className={styles.pokemon}>
-        <p>{item.name}</p>
-        <img src=""/>
-        </div>))}
+                {pokemon.map(item => (
+                    <div key={item.name} className={styles.pokemon}>
+                        <p>{item.name}</p>
+                        <img src="" />
+                    </div>))}
             </div>
             <Pagination
-            nextPage={nextUrl ? nextPage : null}
-            prevPage={previousUrl ? prevPage : null}
+                nextPage={nextUrl ? nextPage : null}
+                prevPage={previousUrl ? prevPage : null}
             />
         </div>
     )
