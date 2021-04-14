90727ec17463508001a46baf52a94bf83b74a53d
Ygor Ferreira <ygor.pinto@hotmail.com>
2021-04-14 12:38:33 -0300

    ok

--- components/PokeInfo/PokeInfoStyles.ts
+++ components/PokeInfo/PokeInfoStyles.ts
@@ -29,17 +29,16 @@ text-transform: capitalize;
 
 .pokemon {
     display:flex;
+    
 }
 
 button {
-    top:50rem;
     background:none;
     border:none;
     color:${props=>props.theme.colors.text};
     font-family: 'Roboto', sans-serif;
     font-weight: 600;
-    top:6%;
-    right:6%;
+    font-size:1.3rem;
 }
 
 header {
@@ -59,6 +58,52 @@ grid-template-columns:1fr;
 align-content:right;
 justify-items:right;
 width:100%;
+}
+
+@media screen and (max-width:500px) {
+
+.pokemonContainerInfo {
+width:90%;
+height:85vh;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+background-color:${props=>props.theme.colors.background};
+color:${props=>props.theme.colors.text};
+top:15%;
+left:30%;
+border-radius:2rem;
+font-family: 'Roboto', sans-serif;
+font-weight: 600;
+text-transform: capitalize;
+font-size:1rem;
+}
+
+header {
+
+h2 {
+    margin:0 3rem 1rem 0;
+    font-size:1rem;
+}
+
+font-size:1rem;
+display:flex;
+flex-direction:column;
+margin:0 3rem 0 0;
+}
+
+.pokemon {
+    display:flex;
+    font-size:1rem;
+}
+
+.stats {
+    p {
+        font-size:0.6rem;
+    }
+}
+
 }
 
 `
